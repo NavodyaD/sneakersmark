1cbb6868394713290f0d5cbcefe969bbb537e4db
Navodya De Silva <125857483+NavodyaD@users.noreply.github.com>
2024-10-10 18:54:37 +0530

    Signin Pages
    Signin Page / Enter Password Page

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Colors.blue;
-  static const background = Colors.white;
-  static const secondBackground = Colors.grey;
+  static const primary = Color(0xff377ba7);
+  static const background = Color(0xff09283e);
+  static const secondBackground = Color(0x6650636f);
 }
--- lib/presentation/auth/pages/signin.dart
+++ lib/presentation/auth/pages/signin.dart
@@ -1,10 +1,76 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:sneakersmark/common/helper/navigator/app_navigator.dart';
+import 'package:sneakersmark/common/widgets/button/basic_app_button.dart';
+import 'package:sneakersmark/presentation/auth/pages/enter_password.dart';
 
 class SigninPage extends StatelessWidget {
   const SigninPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 80,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _signinText(context),
+            const SizedBox(height: 20,),
+            _emailField(context),
+            const SizedBox(height: 20,),
+            _continueButton(context),
+            const SizedBox(height: 20,),
+            _createAccount(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return const Text('Sign in', style: TextStyle(
+      fontSize: 32, fontWeight: FontWeight.bold
+    ),);
+  }
+
+  Widget _emailField(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: 'Enter Email',
+      ),
+    );
+  }
+
+  Widget _continueButton(BuildContext context) {
+    return BasicAppButton(onPressed: () {
+      AppNavigator.pushReplacement(context, const EnterPasswordPage()
+      );
+    },
+    title: 'Continue',);
+  }
+  
+  Widget _createAccount(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          const TextSpan(
+            text: "Don't you have an account?"
+          ),
+          TextSpan(
+              text: 'Create One',
+            recognizer: TapGestureRecognizer()..onTap = () {
+
+            },
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            )
+          )
+        ]
+      ),
+    );
   }
 }
